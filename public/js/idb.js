316da5e9f5969e77db90f04e174bb30c699cdefc
Scott Obregon <obregon.scott@gmail.com>
2022-07-31 22:46:42 -0500

    pop total and pop table

--- public/js/idb.js
+++ public/js/idb.js
@@ -3,26 +3,34 @@ let myChart;
 
 // Fetch api transaction
 fetch("/api/transaction")
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-    // save db data on global variable
-    transactions = data;
-
-    populateTotal();
-    populateTable();
-    populateChart();
-  });
-
-  function populateTotal() {
-
-  }
-
-  function populateTable() {
-  
-  }
-  
-  function populateChart() {
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        transactions = data;
+        popTotal();
+        popTable();
+        popChart();
+    });
+// POP TOTAL
+function popTotal() {
+  let total = transactions.reduce((total, t) => {
+    return total + parseInt(t.value);
+  }, 0);
+  let totalEl = document.querySelector("#total");
+  totalEl.textContent = total;
+}
+// POP TABLE
+function popTable() {
+    let tbody = document.querySelector("#tbody");
+    tbody.innerHTML = "";
+    transactions.forEach(transaction => {
+      let tr = document.createElement("tr");
+      tr.innerHTML = `<td>${transaction.name}</td><td>${transaction.value}</td>`;
+      tbody.appendChild(tr);
+    });
+}
+//   POP CHART
+function pChart() {
 
 }
