9ea18f8d6bdfc661c929d9303db1ce5f6fdce775
Scott Obregon <obregon.scott@gmail.com>
2022-07-31 23:01:22 -0500

    added transaction upload and record save

--- public/js/idb.js
+++ public/js/idb.js
@@ -3,18 +3,63 @@ let db;
 const request = indexedDB.open('budget', 1);
 
 // if successful
-request.onsuccess = function(event) {
+request.onsuccess = function (event) {
     db = event.target.result;
     if (navigator.onLine) {
         uploadTransaction();
     }
 };
 // version changer
-request.onupgradeneeded = function(event) {
+request.onupgradeneeded = function (event) {
     const db = event.target.result;
     db.createObjectStore('new_transaction', { autoIncrement: true });
 };
 // on ERROR
-request.onerror = function(event) {
+request.onerror = function (event) {
     console.log(event.target.errorCode);
 };
+
+
+
+function saveRecord(record) {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+    transactionObjectStore.add(record);
+};
+//  SAVE YOUR RECORDS
+function saveRecord(record) {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+    transactionObjectStore.add(record);
+};
+// Transaction UPLOAD
+function uploadTransaction() {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+    const getAll = transactionObjectStore.getAll();
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }})
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    const transaction = db.transaction(['new_transaction'], 'readwrite');
+                    const transactionObjectStore = transaction.objectStore('new_transaction');
+                    transactionObjectStore.clear();
+                    alert('All saved transactions has been submitted!');
+                })
+                // CATCH ERROR
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    };
+}
