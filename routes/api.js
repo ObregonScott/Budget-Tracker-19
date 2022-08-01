b19a95c2ce77f5d72c1620272001257430482587
Scott Obregon <obregon.scott@gmail.com>
2022-07-31 22:08:35 -0500

    working on bulk api, need documentation but should look the same as above

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,19 @@
 const router = require("express").Router();
 const Transaction =require('../model/transaction.js');
 
+// router post body
 router.post("/api/transaction", ({body}, res) => {
-    
-})
+    Transaction.create(body)
+    .then(dbTransaction => {
+        res.json(dbTransaction)
+    })
+    .catch(err => {
+        res.status(404).json(err)
+    })
+});
+// router post bulk
+router.post("/api/transaction/bulk", ({body}, res) => {
+   
+  });
+
+//   router get
