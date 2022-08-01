fa6809a6bce707364c595cb17aae34f24c87ed51
Scott Obregon <obregon.scott@gmail.com>
2022-07-31 22:32:25 -0500

    install,activate,fetch CACHE, need more doc

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,20 @@
 const APP_PREFIX = 'BudgetTracker-';     
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
+
 const FILES_TO_CACHE = [
-    
+
 ]
+
+// listen install
+self.addEventListener('install', function (e) {
+
+})
+// listen ACTIVATE
+self.addEventListener('activate', function(e) {
+
+});
+// listen FETCH
+self.addEventListener('fetch', function (e) {
+
+})
