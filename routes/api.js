b1e55a9e62372d0368e60ed3cd86dd3efe5de30f
Scott Obregon <obregon.scott@gmail.com>
2022-07-31 22:12:00 -0500

    found documentation for api buld and route get on stackOverflow, tailored for MY APP

--- routes/api.js
+++ routes/api.js
@@ -1,24 +1,36 @@
 const router = require("express").Router();
-const Transaction =require('../model/transaction.js');
+const Transaction = require('../model/transaction.js');
 
 // router post body
-router.post("/api/transaction", ({body}, res) => {
+router.post("/api/transaction", ({ body }, res) => {
     Transaction.create(body)
-    .then(dbTransaction => {
-        res.json(dbTransaction)
-    })
-    .catch(err => {
-        res.status(404).json(err)
-    })
+        .then(dbTransaction => {
+            res.json(dbTransaction)
+        })
+        .catch(err => {
+            res.status(404).json(err)
+        })
 });
 // router post bulk
-router.post("/api/transaction/bulk", ({body}, res) => {
-   
-  });
+router.post("/api/transaction/bulk", ({ body }, res) => {
+    Transaction.insertMany(body)
+        .then(dbTransaction => {
+            res.json(dbTransaction);
+        })
+        .catch(err => {
+            res.status(404).json(err);
+        });
+});
 
 //   router get
 router.get("/api/transaction", (req, res) => {
- 
-  });
+    Transaction.find({}).sort({ date: -1 })
+        .then(dbTransaction => {
+            res.json(dbTransaction);
+        })
+        .catch(err => {
+            res.status(404).json(err);
+        });
+});
 
-  module.exports = router;
+module.exports = router;
